5ea3b1a07d2eeb548e079bd2e7369bd95ab6026c
hari-19 <hari19@outlook.in>
2022-02-14 15:01:16 +0530

    Doctor contract

---
+++ contracts/doctor.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.8.7;
+
+contract Doctor {
+    mapping (string=> string) doctors;
+    mapping (string=> string) hospitals;
+    
+    function postRecord(string memory s, string memory h, string memory key) public {
+        require(keccak256(bytes(doctors[key])) == keccak256(bytes("")), "Duplicate key");
+        doctors[key] = s;
+        hospitals[key] = h;
+    }
+    
+    function updateHostpital(string memory h, string memory key) public {
+        hospitals[key] = h;
+    }
+
+    function getDoctor(string memory key) public view returns(string memory) {
+        return doctors[key];
+    }
+
+    function getHospital(string memory key) public view returns(string memory) {
+        return hospitals[key];
+    }
+}
