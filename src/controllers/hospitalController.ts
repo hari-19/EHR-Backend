b92f6b925b6195588c2b7ccdce400ea1e6c8cd45
hari-19 <hari19@outlook.in>
2022-02-17 12:07:03 +0530

    Get all hospital

--- src/controllers/hospitalController.ts
+++ src/controllers/hospitalController.ts
@@ -1,76 +1,89 @@
-import { Request, Response } from 'express';
-import { Joi } from 'express-validation';
-import { HospitalModel } from '../schemas/hospital';
-import GunDB from '../gun';
+import { Request, Response } from "express";
+import { Joi } from "express-validation";
+import { HospitalModel } from "../schemas/hospital";
+import GunDB from "../gun";
+require("gun/lib/then.js");
 
-export async function getHospitals (req: Request, res: Response, next: any) {
-    try {
-        const hospitals = await HospitalModel.find();
-        res.json({
-            data: hospitals
-        });
-    }
-    catch(error) {
-        next(error);
-    }
+export async function getHospitals(req: Request, res: Response, next: any) {
+  try {
+    const list: any[] = [];
+    const hospitalNode = GunDB.root.get("EHR-Hospital");
+    hospitalNode.once(async (node) => {
+      const keys = Object.keys(node);
+      for (const key of keys) {
+        if (key === "_") continue;
+
+        const d = await hospitalNode.get(key).promise();
+        const details = {
+          id: d.put.id,
+          name: d.put.name,
+          url: d.put.name,
+        };
+        list.push(details);
+      }
+      res.json({
+        data: list,
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export const addHospitalValidation = {
-    body: Joi.object({
-         id: Joi.string().required(),
-         name: Joi.string().required(),
-         url: Joi.string().required()
-     }),
+  body: Joi.object({
+    id: Joi.string().required(),
+    name: Joi.string().required(),
+    url: Joi.string().required(),
+  }),
 };
 
-export async function addHospital (req: Request, res: Response, next: any) {
-    try {
-        const {id, name, url} = req.body;
-        await HospitalModel.create({
-            _id: id,
-            name,
-            url
-        })
-        res.status(201).send("Record Created");
-    }
-    catch(error) {
-        next(error);
-    }
+export async function addHospital(req: Request, res: Response, next: any) {
+  try {
+    const { id, name, url } = req.body;
+    await HospitalModel.create({
+      _id: id,
+      name,
+      url,
+    });
+    res.status(201).send("Record Created");
+  } catch (error) {
+    next(error);
+  }
 }
-
 
 export const addGunHospitalValidation = {
-    body: Joi.object({
-         id: Joi.string().required(),
-         name: Joi.string().required(),
-         url: Joi.string().required()
-     }),
+  body: Joi.object({
+    id: Joi.string().required(),
+    name: Joi.string().required(),
+    url: Joi.string().required(),
+  }),
 };
 
-export async function addGunHospital (req: Request, res: Response, next: any) {
-    try {
-        const {id, name, url} = req.body;
-        GunDB.root.get('EHR-Hospital').get(id).put({id, name, url});
-        res.status(201).send("Record Created");
-    }
-    catch(error) {
-        next(error);
-    }
+export async function addGunHospital(req: Request, res: Response, next: any) {
+  try {
+    const { id, name, url } = req.body;
+    GunDB.root.get("EHR-Hospital").get(id).put({ id, name, url });
+    res.status(201).send("Record Created");
+  } catch (error) {
+    next(error);
+  }
 }
 
-
 export const getGunHospitalValidation = {
-    body: Joi.object({
-         id: Joi.string().required(),
-     }),
+  body: Joi.object({
+    id: Joi.string().required(),
+  }),
 };
 
-export async function getGunHospital (req: Request, res: Response, next: any) {
-    try {
-        const {id } = req.body;
-        GunDB.root.get('EHR-Hospital').get(id).once(node => res.status(200).send(node));
-    }
-    catch(error) {
-        next(error);
-    }
+export async function getGunHospital(req: Request, res: Response, next: any) {
+  try {
+    const { id } = req.body;
+    GunDB.root
+      .get("EHR-Hospital")
+      .get(id)
+      .once((node) => res.status(200).send(node));
+  } catch (error) {
+    next(error);
+  }
 }
